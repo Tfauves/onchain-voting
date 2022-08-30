e77c0842b9af5f2eb454ae6558159b76cf8d50ff
Tranquility Base <tfauvelle@gmail.com>
2022-08-30 10:02:26 -0400

    passes tests

---
+++ programs/onchain-voting/src/lib.rs
@@ -0,0 +1,60 @@
+use anchor_lang::prelude::*;
+
+declare_id!("7W9CKE2Az6zxdFryHZzMJrz89iJLd7WSWkaxx5tXn7Tx");
+
+#[program]
+pub mod onchain_voting {
+    use super::*;
+
+    pub fn init_vote_bank(ctx: Context<InitVote>) -> Result<()> {
+        ctx.accounts.vote_account.is_open_to_vote = true;
+        Ok(())
+    }
+
+    pub fn gib_vote(ctx: Context<GibVote>, vote_type: VoteType) ->  Result<()> {
+        match vote_type {
+            VoteType::GM => {
+                msg!("Voted for GM");
+                ctx.accounts.vote_account.gm += 1;
+            },
+            VoteType::GN => {
+                msg!("Voted for GN");
+                ctx.accounts.vote_account.gn += 1;
+            },
+        };
+        Ok(())
+    }
+}
+
+
+
+#[derive(Accounts)]
+pub struct InitVote<'info> {
+    #[account(init, payer = signer, space = 8 + 1 + 8 + 8,)]
+    pub vote_account: Account<'info, VoteBank>,
+     
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct VoteBank {
+    is_open_to_vote: bool,
+    gm: u64,
+    gn: u64,
+}
+
+#[derive(Accounts)]
+pub struct GibVote<'info> {
+    #[account(mut)]
+    pub vote_account: Account<'info, VoteBank>,
+    pub signer: Signer<'info>,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub enum VoteType {
+    GM,
+    GN
+}
---
+++ tests/onchain-voting.ts
@@ -0,0 +1,50 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { OnchainVoting } from "../target/types/onchain_voting";
+
+describe("onchain-voting", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.OnchainVoting as Program<OnchainVoting>;
+  let voteBank = anchor.web3.Keypair.generate();
+
+  it("Creates a vote bank for the public to vote", async () => {
+    // Add your test here.
+    const tx = await program.methods.initVoteBank()
+    .accounts({
+      voteAccount: voteBank.publicKey,
+    })
+    .signers([voteBank])
+    .rpc();
+    console.log("TxHash ::", tx);
+  });
+
+  it("Votes for GM", async () => {
+    const tx = await program.methods.gibVote({gm:{}})
+    .accounts({
+      voteAccount: voteBank.publicKey,
+    })
+    .rpc();
+    console.log("TxHash ::", tx);
+
+    let voteBankData = await program.account.voteBank.fetch(voteBank.publicKey);
+    console.log(`Total GMs :: ${voteBankData.gm}`)
+    console.log(`Total GNs :: ${voteBankData.gn}`)
+
+  });
+
+  it("Votes for GN", async () => {
+    const tx = await program.methods.gibVote({gn:{}})
+    .accounts({
+      voteAccount: voteBank.publicKey,
+    })
+    .rpc();
+    console.log("TxHash ::", tx);
+
+    let voteBankData = await program.account.voteBank.fetch(voteBank.publicKey);
+    console.log(`Total GMs :: ${voteBankData.gm}`)
+    console.log(`Total GNs :: ${voteBankData.gn}`)
+  });
+
+});
